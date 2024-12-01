b1730d953574f283e2ddf65eb67a3d52f24e2796
hadawiyah <wiyah0852@gmail.com>
2024-12-02 03:26:39 +0700

    Update dashboard-child.php

--- tLogin/dashboard-child.php
+++ tLogin/dashboard-child.php
@@ -1,10 +1,4 @@
 <?php
-session_start();
-if (!isset($_SESSION['role']) || $_SESSION['role'] != 'children') {
-    header("Location: login.php");  // Jika bukan anak, arahkan ke halaman login
-    exit;
-}
-
 include "template/mainheader.php";
 ?>
 
