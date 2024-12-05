b41532f3f923b589ed7d447e947048e441fb5abd
hadawiyah <wiyah0852@gmail.com>
2024-12-05 12:39:37 +0700

    Update config.php

--- tLogin/config.php
+++ tLogin/config.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $password = "";
-$dbname = "keuangan";
+$dbname = "financeManager";
 
 // Membuat koneksi ke database
 $conn = mysqli_connect($host, $user, $password, $dbname);
