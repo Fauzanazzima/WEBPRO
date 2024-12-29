64d7e86992b0244c8d034d840ad7b4e8b762d797
hadawiyah <wiyah0852@gmail.com>
2024-12-29 19:42:52 +0700

    Update config.php

--- tLogin/config.php
+++ tLogin/config.php
@@ -1,14 +1,15 @@
 <?php
+// Koneksi ke database dengan PDO
 $host = "localhost";
 $user = "root";
 $password = "";
 $dbname = "financeManager";
 
-// Membuat koneksi ke database
-$conn = mysqli_connect($host, $user, $password, $dbname);
-
-// Mengecek koneksi
-if (!$conn) {
-    die("Koneksi gagal: " . mysqli_connect_error());
+// Membuat koneksi ke database menggunakan PDO
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  // Menampilkan error jika ada masalah
+} catch (PDOException $e) {
+    die("Koneksi gagal: " . $e->getMessage());
 }
 ?>
